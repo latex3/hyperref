9914b876b77e7e99590387b4075abeebeeae9fdb
David Carlisle <d.p.carlisle@gmail.com>
2019-09-28 15:42:07 +0100

    start of ctan build settings

--- build.lua
+++ build.lua
@@ -2,13 +2,18 @@
 
 -- Build script for hyperref  (check only)
 module = "hyperref"
---sourcefiles = {'*.dtx'}
+
 installfiles = {'*.def','*.sty'}
 
 checkconfigs = {"build","config-3","config-xetex"}
 checkengines = {"pdftex","luatex"}
 checkruns = 2
 
+-- start at getting cyan build organised, not complete yet (mkctan script still used)
+sourcefiles        = {"*.dtx", "*.ins", "bmhydoc.sty","*-hyper.sty","paperslides99.zip","psdmapshortnames.pl","doc/*.???"}
+typesetfiles={"doc/manual.tex","doc/options.tex","backref.dtx","hyperref.dtx","nameref.dtx"
+
+flatten = false
 
 kpse.set_program_name ("kpsewhich")
 if not release_date then

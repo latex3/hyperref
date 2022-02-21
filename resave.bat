dd16c7dcffbe25d346ed85842d95fa09348f3f14
Ulrike Fischer <fischer@troubleshooting-tex.de>
2022-02-21 12:56:03 +0100

    test failures

--- resave.bat
+++ resave.bat
@@ -1,3 +1,68 @@
 
 rem */\(1+[a-z0-9\-]\)\.\(2+[a-z]\)\.*
 rem l3build save -e\2 \1
+
+
+
+  Check failed with difference files
+  - ./build/test/gh118-xr-hyper.etex.fc
+  - ./build/test/gh118-xr-hyper.luatex.fc
+  - ./build/test/gh118-xr-hyper.pdftex.fc
+  - ./build/test/hyperget-test.etex.fc
+  - ./build/test/hyperget-test.luatex.fc
+  - ./build/test/hyperget-test.pdftex.fc
+  - ./build/test/next-anchor.etex.fc
+  - ./build/test/next-anchor.luatex.fc
+  - ./build/test/next-anchor.pdftex.fc
+  - ./build/test/o50-footnotesspaces.etex.fc
+  - ./build/test/o50-footnotesspaces.luatex.fc
+  - ./build/test/o50-footnotesspaces.pdftex.fc
+  - ./build/test/test1.etex.fc
+  - ./build/test/test1.luatex.fc
+  - ./build/test/test1.pdftex.fc
+
+Failed tests for configuration config-pvt:
+
+  Check failed with difference files
+  - ./build/test-config-pvt/25-sec-non-ascii.luatex.fc
+  - ./build/test-config-pvt/25-sec-non-ascii.pdftex.fc
+  - ./build/test-config-pvt/40-test-pdfhighlight.luatex.fc
+  - ./build/test-config-pvt/40-test-pdfhighlight.pdftex.fc
+  - ./build/test-config-pvt/87-pdfversion.luatex.fc
+  - ./build/test-config-pvt/87-pdfversion.pdftex.fc
+  - ./build/test-config-pvt/issue49-textfields-utf8.luatex.fc
+  - ./build/test-config-pvt/issue49-textfields-utf8.pdftex.fc
+  - ./build/test-config-pvt/issue93-formfieldspace.luatex.fc
+  - ./build/test-config-pvt/issue93-formfieldspace.pdftex.fc
+  - ./build/test-config-pvt/latex2e.luatex.fc
+  - ./build/test-config-pvt/latex2e.pdftex.fc
+  - ./build/test-config-pvt/test-bm-pu-licr.luatex.fc
+  - ./build/test-config-pvt/test-bm-pu-licr.pdftex.fc
+  - ./build/test-config-pvt/test0-pdf.luatex.fc
+  - ./build/test-config-pvt/test0-pdf.pdftex.fc
+  - ./build/test-config-pvt/test4-pdf.luatex.fc
+  - ./build/test-config-pvt/test4-pdf.pdftex.fc
+  - ./build/test-config-pvt/test6-pdf.luatex.fc
+  - ./build/test-config-pvt/test6-pdf.pdftex.fc
+  - ./build/test-config-pvt/test8.luatex.fc
+  - ./build/test-config-pvt/test8.pdftex.fc
+  - ./build/test-config-pvt/testams.luatex.fc
+  - ./build/test-config-pvt/testams.pdftex.fc
+  - ./build/test-config-pvt/testbmgl.luatex.fc
+  - ./build/test-config-pvt/testbmgl.pdftex.fc
+  - ./build/test-config-pvt/testbmu.luatex.fc
+  - ./build/test-config-pvt/testbmu.pdftex.fc
+  - ./build/test-config-pvt/testbookmark.luatex.fc
+  - ./build/test-config-pvt/testbookmark.pdftex.fc
+  - ./build/test-config-pvt/testform.luatex.fc
+  - ./build/test-config-pvt/testform.pdftex.fc
+  - ./build/test-config-pvt/testnb-pdf.luatex.fc
+  - ./build/test-config-pvt/testnb-pdf.pdftex.fc
+  - ./build/test-config-pvt/unicode-test.luatex.fc
+  - ./build/test-config-pvt/unicode-test.pdftex.fc
+
+Failed tests for configuration config-3:
+
+  Check failed with difference files
+  - ./build/test-config-3/test1-pdf.luatex.fc
+  - ./build/test-config-3/test1-pdf.pdftex.fc
